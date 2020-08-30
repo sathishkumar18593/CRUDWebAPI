a6adb02e457c04e6f38f104f9b3fd5aebcf5fac8
sathishkumar18593 <70486995+sathishkumar18593@users.noreply.github.com>
2020-08-31 00:49:53 +0530

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace CRUD
+{
+    public class Startup
+    {
+
+        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            // services.AddCors();
+            //services.AddCors(options => options.AddPolicy("AllowAll", p => p.AllowAnyOrigin()
+            //                                                         .AllowAnyMethod()
+            //                                                          .AllowAnyHeader()));
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                                  builder =>
+                                  {
+                                      builder.WithOrigins("http://localhost:3000",
+                                                          "https://localhost:12345/api/values");
+                                      builder.AllowAnyHeader();
+                                      builder.AllowAnyMethod();
+                                      builder.AllowCredentials();
+                                  });
+            });
+
+            var connectionstring = Configuration["ConnectionStrings:DefaultConnection"];
+
+            Configuration.GetConnectionString(connectionstring);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+            //app.UseCors(options => options.AllowAnyOrigin());
+            //app.UseCors("AllowAll");
+            app.UseCors(MyAllowSpecificOrigins);
+
+
+
+        }
+    }
+}
