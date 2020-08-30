608ca4a61fcd10f922d46a2f2f37dc7684131f4e
sathishkumar18593 <70486995+sathishkumar18593@users.noreply.github.com>
2020-08-31 00:47:51 +0530

    Add files via upload

---
+++ Models/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD.Models
+{
+    public class Employee
+    {
+        public int? id { get; set; }
+        public string Firstname { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Gender { get; set; }
+
+        public int? Age { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
+        public string Joineddate { get; set; }
+        public string View { get; set; }
+        public string Edit { get; set; }
+        public string Delete { get; set; }
+    }
+}
